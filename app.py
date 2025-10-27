c769a9f69ea5ce8ebd3a4a2b93ae3609f3d3ded4
George M. <32426140+iHeroGH@users.noreply.github.com>
2025-10-27 15:02:16 -0700

    redirect instead of reroute

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import logging
 
-import httpx
-from quart import Quart, Response, render_template
+from quart import Quart, Response, redirect, render_template
 
 app = Quart(__name__)
 logger = app.logger
@@ -25,28 +24,29 @@ async def proxy(subpath: str):
 
 async def reroute_to(url: str, sub: str) -> Response:
     logger.info(f"Attempting re-route to {url}")
-    async with httpx.AsyncClient() as client:
-        resp = await client.get(url)
+    return redirect(url, code=302)  # type: ignore
+    # async with httpx.AsyncClient() as client:
+    #     resp = await client.get(url)
 
-    headers = dict(resp.headers)
-    headers.pop("content-encoding", None)
-    headers.pop("transfer-encoding", None)
-    headers.pop("content-length", None)
+    # headers = dict(resp.headers)
+    # headers.pop("content-encoding", None)
+    # headers.pop("transfer-encoding", None)
+    # headers.pop("content-length", None)
 
-    content = resp.content
-    if "text/html" in resp.headers.get("content-type", ""):
-        content = content.decode("utf-8")
-        content = content.replace(
-            "<head>",
-            f'<head><base href="{sub}">'
-        )
-        content = content.encode("utf-8")
+    # content = resp.content
+    # if "text/html" in resp.headers.get("content-type", ""):
+    #     content = content.decode("utf-8")
+    #     content = content.replace(
+    #         "<head>",
+    #         f'<head><base href="{sub}">'
+    #     )
+    #     content = content.encode("utf-8")
 
-    logger.debug(
-        f"Re-route returned code {resp.status_code} with {content}"
-    )
+    # logger.debug(
+    #     f"Re-route returned code {resp.status_code} with {content}"
+    # )
 
-    return Response(content, resp.status_code, headers=headers)
+    # return Response(content, resp.status_code, headers=headers)
 
 
 if __name__ == '__main__':
